854f67ad463fe6b79a47f58c5cf2a8c5d4797ddc
Rachel Maudlin <Rachel.Maudlin@e-tcc.com>
2020-12-16 14:35:19 -0500

    Testing Commit

---
+++ CafeMenu_Tests/CafeMenuRepositoryTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CafeMenu_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CafeMenu_Tests
+{
+    [TestClass]
+    public class KomodoCafe_RepositoryTests
+    {
+        private CafeMenuRepository _repo;
+        private CafeMenu _item;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new CafeMenuRepository();
+            _item = new CafeMenu(6, "Bix's Buffet", "Endless Food", "Access to the whole buffet and unlimited refills", 25.99);
+            _repo.AddItemToMenu(_item);
+        }
+
+        //ADD METHOD
+        [TestMethod]
+        public void AddToMenu_ShouldGetNotNull()
+        {
+            //ARRANGE
+            CafeMenu item = new CafeMenu();
+            item.MealNumber = 1;
+            CafeMenuRepository repository = new CafeMenuRepository();
+            //ACT
+            repository.AddItemToMenu(item);
+            CafeMenu itemFromDirectory = repository.GetItemByNumber(1);
+            //ASSERT
+            Assert.IsNotNull(itemFromDirectory);
+        }
+        //UPDATE
+        [TestMethod]
+        public void UpdateMenuItem_ShouldReturnNotNull()
+        {
+            //ARRANGE
+            //TEST INITIALIZE
+            CafeMenu newItem = new CafeMenu(6, "Bix's Bountiful Buffet", "Endless Food For One", "Access to the whole buffet and unlimited refills", 25.99);
+            //ACT
+            bool updateResult = _repo.UpdateExistingMenu(6, newItem);
+            //ASSERT
+            Assert.IsTrue(updateResult);
+        }
+
+        [DataTestMethod]
+        [DataRow(6, true)]
+        [DataRow(8, false)]
+        public void UpdateMenuItem_ShouldMatchGivenBool(int originalMealNumber, bool shouldUpdate)
+        {
+            //ARRANGE
+            //TEST INITIALIZE
+            CafeMenu newItem = new CafeMenu(6, "Bix's Bountiful Buffet", "Endless Food For One", "Access to the whole buffet and unlimited refills", 25.99);
+            //ACT
+            bool updateResult = _repo.UpdateExistingMenu(originalMealNumber, newItem);
+            //ASSERT
+            Assert.AreEqual(shouldUpdate, updateResult);
+        }
+
+        [TestMethod]
+        public void DeleteMenuItem_ShouldReturnTrue()
+        {
+            //ARRANGE
+
+            //ACT
+            bool deleteResult = _repo.RemoveItemFromMenu(_item.MealNumber);
+            //ASSERT
+            Assert.IsTrue(deleteResult);
+        }
+    }
+}
---
+++ CafeMenu_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CafeMenu_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CafeMenu_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a0cd5750-f618-4175-805f-c8f62cdd3bc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
