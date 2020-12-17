94d5d540e936c6c98db3b70b83b9d2cb386f1a9c
Rachel Maudlin <Rachel.Maudlin@e-tcc.com>
2020-12-17 10:58:13 -0500

    Badge Class Complete

--- EmployeeBadge_Repository/EmployeeBadge.cs
+++ EmployeeBadge_Repository/EmployeeBadge.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace EmployeeBadge_Repository
 {
+   
     public class EmployeeBadge
     {
+        public int BadgeID { get; set; }
+        public string DoorName { get; set; }
+
+        public EmployeeBadge() { }
+        public EmployeeBadge(int badgeID, string doorName)
+        {
+            BadgeID = badgeID;
+            DoorName = doorName;
+        }
+
     }
 }
