4e046fc7d1345bf86129a3161f455295b84df208
Rachel Maudlin <Rachel.Maudlin@e-tcc.com>
2020-12-17 23:16:07 -0500

    finished all testing

--- ClaimsDepartment_Tests/ClaimsDepartmentRepositoryTesting.cs
+++ ClaimsDepartment_Tests/ClaimsDepartmentRepositoryTesting.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ClaimsDepartment_Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ClaimsDepartment_Tests
@@ -6,9 +8,50 @@ namespace ClaimsDepartment_Tests
     [TestClass]
     public class ClaimsDepartmentRepositoryTesting
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ClaimsDepartmentRepository _repo;
+        private ClaimItems _item;
+        [TestInitialize]
+        public void Arrange()
         {
+            _repo = new ClaimsDepartmentRepository();
+            _item = new ClaimItems(1, TypeOfClaim.Car, "Car accident on 465", 400.00, DateTime.Parse("04/25/20"), DateTime.Parse("04 / 27 / 20"), true);
+            _repo.AddClaimToQueue(_item);
+        }
+        //VIEW ALL CLAIMS
+        [TestMethod]
+        public void ViewAllClaims_NoNullAllowed()
+        {
+            ClaimsDepartmentRepository repository = new ClaimsDepartmentRepository();
+            ClaimItems test = new ClaimItems(1, TypeOfClaim.Car, "Car accident on 465", 400.00, DateTime.Parse("04/25/20"), DateTime.Parse("04 / 27 / 20"), true);
+            repository.AddClaimToQueue(test);
+            var testing = repository.ReadClaimsQueue();
+            Assert.IsNotNull(testing);
+        }
+        //TAKE CARE OF CLAIM
+        [TestMethod]
+        public void PeekAtClaim_ShouldReturnNotNull()
+        {
+            ClaimItems item = new ClaimItems();
+            item.ClaimID = 1;
+            ClaimsDepartmentRepository repository = new ClaimsDepartmentRepository();
+
+            repository.AddClaimToQueue(item);
+            ClaimItems itemsFromQueue = repository.TakeCareOfClaim();
+            Assert.IsNotNull(itemsFromQueue);
+        }
+
+        //CREATE CLAIM
+        [TestMethod]
+        public void AddClaimToQueue_NoNullAllowed()
+        {
+            ClaimItems item = new ClaimItems();
+            item.ClaimID = 1;
+            ClaimsDepartmentRepository repository = new ClaimsDepartmentRepository();
+
+            repository.AddClaimToQueue(item);
+            ClaimItems itemsFromQueue = repository._ClaimsQueue.Peek();
+            Assert.IsNotNull(itemsFromQueue);
+
         }
     }
 }
